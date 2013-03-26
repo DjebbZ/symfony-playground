5b7478855cfdc0e8d9c9938e559da76c6c973c5c
DjebbZ <khalid.jebbari@gmail.com>
2013-03-26 16:39:49 +0100

    Create a dumb route that sends a json response

--- src/DjebbZ/HelloBundle/Controller/DefaultController.php
+++ src/DjebbZ/HelloBundle/Controller/DefaultController.php
@@ -3,11 +3,21 @@
 namespace DjebbZ\HelloBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('DjebbZHelloBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function indexJsonAction($name)
+    {
+      $json = array('name' => $name );
+      $response = new Response(json_encode($json));
+      $response->headers->set('Content-Type', 'application/json');
+
+      return $response;
     }
 }
