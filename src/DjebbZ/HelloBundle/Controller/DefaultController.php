554f59f5b9de6629af7eb159e3b836e622137a30
DjebbZ <khalid.jebbari@gmail.com>
2013-03-26 12:45:30 +0100

    Inital commit, after create of the DjebbZ bundle

---
+++ src/DjebbZ/HelloBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DjebbZ\HelloBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('DjebbZHelloBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/DjebbZ/HelloBundle/DjebbZHelloBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DjebbZ\HelloBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DjebbZHelloBundle extends Bundle
+{
+}
