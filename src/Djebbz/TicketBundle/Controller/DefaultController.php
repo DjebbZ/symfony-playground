08368242bd199200f69c58cffce46f413a48eeaf
DjebbZ <khalid.jebbari@gmail.com>
2013-03-28 13:54:07 +0100

    Renames routes and implement controller to show all tickets

--- src/Djebbz/TicketBundle/Controller/DefaultController.php
+++ src/Djebbz/TicketBundle/Controller/DefaultController.php
@@ -3,11 +3,32 @@
 namespace Djebbz\TicketBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Djebbz\TicketBundle\Model\TicketQuery;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DjebbzTicketBundle:Default:index.html.twig', array('content' => 'Test content'));
+        return $this->render(
+            'DjebbzTicketBundle:Default:index.html.twig',
+            array('content' => 'Test content')
+        );
+    }
+
+    public function showAllTicketAction()
+    {
+        $tickets = TicketQuery::create()
+            ->find();
+
+        if (!$tickets) {
+            throw $this->createNotFoundException(
+                'Sorry, no tickets found. You can create one !'
+            );
+        }
+
+        return $this->render(
+            'DjebbzTicketBundle:Default:showAll.html.twig',
+            array('tickets' => $tickets)
+        );
     }
 }
